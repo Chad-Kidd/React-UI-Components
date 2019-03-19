7653774c12a355a82ed4a76cb38b3be986ce08ce
chad kidd <ckiddwebdev@gmail.com>
2019-03-19 15:15:00 -0700

    buttons without css

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import './App.css';
-import './components/ButtonComponents/ActionButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 import './components/ButtonComponents/NumberButton';
 import './components/DisplayComponents/CalculatorDisplay';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
+      {/* <h3>Welcome to React Calculator</h3> */}
+      <CalculatorDisplay />
+
+      {/* <ActionButton /> */}
+      <ActionButton text="CLEAR" buttonStyle= "white"/>
+
+      <NumberButton text="/" buttonStyle= "red"/>
+
+      {/* <NumberButton /> */}
+        <NumberButton text="7" buttonStyle = "grey"/>
+        <NumberButton text="8" buttonStyle = "grey"/>
+        <NumberButton text="9" buttonStyle = "grey"/>
+        <NumberButton text="x" buttonStyle= "red"/> 
+
+        <NumberButton text="4" buttonStyle = "grey"/>
+        <NumberButton text="5" buttonStyle = "grey"/>
+        <NumberButton text="6" buttonStyle = "grey"/>
+        <NumberButton text="-" buttonStyle= "red"/> 
+
+        <NumberButton text="1" buttonStyle = "grey"/>
+        <NumberButton text="2" buttonStyle = "grey"/>
+        <NumberButton text="3" buttonStyle = "grey"/>
+        <NumberButton text="+" buttonStyle= "red"/> 
+
+      {/* <ActionButton /> */}
+        <ActionButton text="0" buttonStyle= "white"/>
+
+      <NumberButton text="=" buttonStyle= "red"/>
+      {/* <p>
         <strong>
           Don't forget to `default export` your components and import them here
           inside of this file in order to make them work.
         </strong>
-      </p>
+      </p> */}
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Display.css';
 
-const CalculatorDisplay = props => {
-    return <div className="display" > ${props.total}>0</div>;
+const CalculatorDisplay = () => {
+    return <div className="display">0</div>;
 };
 
 export default CalculatorDisplay;
